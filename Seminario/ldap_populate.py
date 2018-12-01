c0f1022472f561d4cf159c38c27d227ec89c320f
Rafael Coelho <rafallcoelho@gmail.com>
2018-12-01 18:07:32 -0300

    Finaliza script de migracao mysql>ldap

--- Seminario/ldap_populate.py
+++ Seminario/ldap_populate.py
@@ -1,12 +1,20 @@
-import csv
 import MySQLdb
 import getpass
 import ldap
+import ldap.modlist
 
-path = '../DATASET/movie_metadata.csv'
+user = raw_input("Digite o usuario de acesso ao banco: ") 
+password = getpass.getpass("Digite a senha: ")
+database = raw_input("Digite o nome do banco: ")
+qt_membros = int(raw_input("Digite o numero maximo de membros do comite: "))
+qt_autores = int(raw_input("Digite o numero maximo de autores: "))
+qt_total = qt_membros + qt_autores
+
+'''
 user = 'root'
 password = ''
 database = 'CASCONet'
+'''
 
 #conexao com o banco
 mydb = MySQLdb.connect(host='localhost',
@@ -16,15 +24,40 @@ mydb = MySQLdb.connect(host='localhost',
 cursor = mydb.cursor()
 
 #consulta por pessoas do banco
-sql_selesct_query = "select * from Person"
+sql_select_query = "select * from Person"
 cursor.execute(sql_select_query)
-records_pessoas = cursor.fetchall()
+pessoas = cursor.fetchall()
 cursor.close()
 
-#conexao com a base ldap
-ldap_con = ldap.initialize(ldap:///localhost)
+if (len(pessoas) < qt_membros + qt_autores):
+    print 'A base mysql tem somente ' + str(len(pessoas)) + ' registros. O numero de membros do comite mais o numero de autores deve ser menor que a quantidade de registros.'
+    exit()
 
-for row in records_pessoas:
-	#insere id row[0]
-	#insere nome de row[1]
-	#insere sobrenome de row[2] 
+#divisao das pessoas do banco entre membros do comite e autores
+membrosComite = pessoas[:qt_membros]
+autores = pessoas[qt_membros:qt_total]
+
+#conexao com a base ldap
+ldap_con = ldap.initialize('ldap:///localhost')
+
+#insersao de membros do comite na base ldap
+for row in membrosComite:
+    dn = "uid=" + str(row[0]) + ',ou=membrosComite,ou=participantes,ou=2018,dc=congresso'
+    modlist = {
+            "uid": str([row[0]]),
+            "name": [row[1]],
+            "lastname": [row[2]]
+            }
+    result = ldap_con.add(dn, ldap.modlist.addModlist(modlist))
+    print result
+
+#insersao de autores na base ldap
+for row in autores:
+    dn = "uid=" + str(row[0]) + ',ou=autores,ou=participantes,ou=2018,dc=congresso'
+    modlist = {
+            "uid": str([row[0]]),
+            "name": [row[1]],
+            "lastname": [row[2]]
+            }
+    result = ldap_con.add(dn, ldap.modlist.addModlist(modlist))
+    print result
